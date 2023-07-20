c9c75ea6797151c79f098b76674956b22d54f8d4
Giovanni Sileno <g.sileno@uva.nl>
2023-07-20 14:56:36 +0200

    started refactoring the visualization library. plenty to do.

--- mesa_gym/common/data_viz.py
+++ mesa_gym/common/data_viz.py
@@ -1,64 +1,136 @@
-import os
-path = os.path.dirname(os.path.abspath(__file__))
+class DataViz:
 
-experiment_file = "lumberjack-qlearning_3_1000_0.05_1.0_0.002_0.1.pickle"
-view_fields = ["reward", "failure"]  # at least two fields, otherwise there are problems.
+    def __init__(self, experiment_file, view = "per_episode"):
 
-import pickle
-with open(f"{path}/{experiment_file}", "rb") as f:
-    data = pickle.load(f)
+        import pickle
+        with open(f"{experiment_file}", "rb") as f:
+            data = pickle.load(f)
 
-data_fields = data["fields"] + ["reward"]
-del(data["fields"])
+        data_fields = data["fields"] + ["reward"]
+        del(data["fields"])
 
-# data processing for visualization
+        # data processing for visualization
+        rows = []
 
-rows = []
-for episode in data.keys():
-    for step in data[episode].keys():
-        for agent in data[episode][step]:
-            row = {}
-            row["agent"] = agent
-            row["episode"] = episode
-            row["step"] = step
-            row["reward"] = data[episode][step][agent]["reward"]
-            for key in data_fields:
-                if key in data[episode][step][agent]:
-                    row[key] = data[episode][step][agent][key]
-                else:
-                    row[key] = 0
-            rows.append(row)
+        if view == "per_step":
+            for episode in data.keys():
+                for step in data[episode].keys():
+                    for agent in data[episode][step]:
+                        row = {}
+                        row["agent"] = agent
+                        row["episode"] = episode
+                        row["step"] = step
+                        row["reward"] = data[episode][step][agent]["reward"]
+                        for key in data_fields:
+                            if key in data[episode][step][agent]:
+                                row[key] = data[episode][step][agent][key]
+                            else:
+                                row[key] = 0
+                        rows.append(row)
+        elif view == "per_episode":
+            for episode in data.keys():
+                agents = []
+                rewards_per_agent = {}
+                steps_per_agent = {}
+                for step in data[episode].keys():
+                    for agent in data[episode][step]:
+                        if agent not in agents:
+                            agents.append(agent)
+                        if agent not in rewards_per_agent:
+                            rewards_per_agent[agent] = 0
+                        if agent not in steps_per_agent:
+                            steps_per_agent[agent] = 0
+                        steps_per_agent[agent] += 1
+                        rewards_per_agent[agent] += data[episode][step][agent]["reward"]
 
-import pandas as pd
-df = pd.DataFrame(rows)
+                for agent in agents:
+                    row = {}
+                    row["episode"] = episode
+                    row["agent"] = agent
+                    row["reward"] = rewards_per_agent[agent]
+                    row["n_steps"] = steps_per_agent[agent]
+                    row["average_reward"] = row["reward"]/row["n_steps"]
+                    rows.append(row)
+        elif view == "per_episode_group":
+            for i in range(10):
+                agents_in_group = []
+                rewards_per_agent_in_group = {}
+                steps_per_agent_in_group = {}
 
-import matplotlib
-matplotlib.use('TkAgg')
-import matplotlib.pyplot as plt
+                for episode in data.keys():
+                    agents = []
+                    rewards_per_agent = {}
+                    steps_per_agent = {}
+                    for step in data[episode].keys():
+                        for agent in data[episode][step]:
+                            if agent not in agents_in_group:
+                                agents_in_group.append(agent)
+                            if agent not in agents:
+                                agents.append(agent)
+                            if agent not in rewards_per_agent:
+                                rewards_per_agent[agent] = 0
+                            if agent not in steps_per_agent:
+                                steps_per_agent[agent] = 0
+                            steps_per_agent[agent] += 1
+                            rewards_per_agent[agent] += data[episode][step][agent]["reward"]
 
-episodes = df.episode.unique()
-agents = df.agent.unique()
+                    for agent in agents:
+                        if agent not in rewards_per_agent_in_group:
+                            rewards_per_agent_in_group[agent] = 0
+                        if agent not in steps_per_agent_in_group:
+                            steps_per_agent_in_group[agent] = 0
 
-fig, axs = plt.subplots(ncols=len(view_fields), nrows=len(agents), figsize=(12, 5))
+                        rewards_per_agent_in_group[agent] += rewards_per_agent[agent]
+                        steps_per_agent_in_group[agent] += steps_per_agent[agent]
 
-for i, agent in enumerate(agents):
-    x = []
-    y = {}
-    for key in view_fields:
-        y[key] = []
+                for agent in agents_in_group:
+                    row = {}
+                    row["episode_group"] = i
+                    row["agent"] = agent
+                    row["reward"] = rewards_per_agent_in_group[agent]
+                    row["n_steps"] = steps_per_agent_in_group[agent]
+                    row["average_reward"] = row["reward"]/row["n_steps"]
+                    rows.append(row)
+        else:
+            raise RuntimeError(f"View {view} unknown!")
 
-    for episode in episodes:
-        x.append(episode)
-        Y = df[(df["agent"] == agent) & (df["episode"] == episode)][view_fields].sum()
-        for key in view_fields:
-            y[key].append(Y[key])
+        import pandas as pd
+        self.df = pd.DataFrame(rows)
 
-    # Plot outcomes
-    for j, key in enumerate(view_fields):
-        axs[i][j].set_title(f"agent {agent}")
-        axs[i][j].set_xlabel("episode number")
-        axs[i][j].set_ylabel(key)
-        axs[i][j].plot(x, y[key])
+    def show(self, view_fields = ("reward", "failure")):
 
-plt.tight_layout()
-plt.show()
+        if len(view_fields) < 2:
+            raise RuntimeError("Please furnish at least two view fields.")
+
+        import matplotlib
+        matplotlib.use('TkAgg')
+        import matplotlib.pyplot as plt
+
+        df = self.df
+
+        episodes = df.episode.unique()
+        agents = df.agent.unique()
+
+        fig, axs = plt.subplots(ncols=len(view_fields), nrows=len(agents), figsize=(12, 5))
+
+        for i, agent in enumerate(agents):
+            x = []
+            y = {}
+            for key in view_fields:
+                y[key] = []
+
+            for episode in episodes:
+                x.append(episode)
+                Y = df[(df["agent"] == agent) & (df["episode"] == episode)][view_fields].sum()
+                for key in view_fields:
+                    y[key].append(Y[key])
+
+            # Plot outcomes
+            for j, key in enumerate(view_fields):
+                axs[i][j].set_title(f"agent {agent}")
+                axs[i][j].set_xlabel("episode number")
+                axs[i][j].set_ylabel(key)
+                axs[i][j].plot(x, y[key])
+
+        plt.tight_layout()
+        plt.show()
