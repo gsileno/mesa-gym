6090538badee32ee46369edcbadba61bd9da858e
Giovanni Sileno <g.sileno@uva.nl>
2023-08-17 16:55:06 +0200

    simplified version -- to be adjusted yet

--- mesa_gym/common/data_viz.py
+++ mesa_gym/common/data_viz.py
@@ -1,6 +1,6 @@
 class DataViz:
 
-    def __init__(self, experiment_file, view = "per_episode"):
+    def __init__(self, experiment_file):
 
         import pickle
         with open(f"{experiment_file}", "rb") as f:
@@ -12,92 +12,25 @@ class DataViz:
         # data processing for visualization
         rows = []
 
-        if view == "per_step":
-            for episode in data.keys():
-                for step in data[episode].keys():
-                    for agent in data[episode][step]:
-                        row = {}
-                        row["agent"] = agent
-                        row["episode"] = episode
-                        row["step"] = step
-                        row["reward"] = data[episode][step][agent]["reward"]
-                        for key in data_fields:
-                            if key in data[episode][step][agent]:
-                                row[key] = data[episode][step][agent][key]
-                            else:
-                                row[key] = 0
-                        rows.append(row)
-        elif view == "per_episode":
-            for episode in data.keys():
-                agents = []
-                rewards_per_agent = {}
-                steps_per_agent = {}
-                for step in data[episode].keys():
-                    for agent in data[episode][step]:
-                        if agent not in agents:
-                            agents.append(agent)
-                        if agent not in rewards_per_agent:
-                            rewards_per_agent[agent] = 0
-                        if agent not in steps_per_agent:
-                            steps_per_agent[agent] = 0
-                        steps_per_agent[agent] += 1
-                        rewards_per_agent[agent] += data[episode][step][agent]["reward"]
-
-                for agent in agents:
+        for episode in data.keys():
+            for step in data[episode].keys():
+                for agent in data[episode][step]:
                     row = {}
+                    row["agent"] = agent
                     row["episode"] = episode
-                    row["agent"] = agent
-                    row["reward"] = rewards_per_agent[agent]
-                    row["n_steps"] = steps_per_agent[agent]
-                    row["average_reward"] = row["reward"]/row["n_steps"]
+                    row["step"] = step
+                    row["reward"] = data[episode][step][agent]["reward"]
+                    for key in data_fields:
+                        if key in data[episode][step][agent]:
+                            row[key] = data[episode][step][agent][key]
+                        else:
+                            row[key] = 0
                     rows.append(row)
-        elif view == "per_episode_group":
-            for i in range(10):
-                agents_in_group = []
-                rewards_per_agent_in_group = {}
-                steps_per_agent_in_group = {}
-
-                for episode in data.keys():
-                    agents = []
-                    rewards_per_agent = {}
-                    steps_per_agent = {}
-                    for step in data[episode].keys():
-                        for agent in data[episode][step]:
-                            if agent not in agents_in_group:
-                                agents_in_group.append(agent)
-                            if agent not in agents:
-                                agents.append(agent)
-                            if agent not in rewards_per_agent:
-                                rewards_per_agent[agent] = 0
-                            if agent not in steps_per_agent:
-                                steps_per_agent[agent] = 0
-                            steps_per_agent[agent] += 1
-                            rewards_per_agent[agent] += data[episode][step][agent]["reward"]
-
-                    for agent in agents:
-                        if agent not in rewards_per_agent_in_group:
-                            rewards_per_agent_in_group[agent] = 0
-                        if agent not in steps_per_agent_in_group:
-                            steps_per_agent_in_group[agent] = 0
-
-                        rewards_per_agent_in_group[agent] += rewards_per_agent[agent]
-                        steps_per_agent_in_group[agent] += steps_per_agent[agent]
-
-                for agent in agents_in_group:
-                    row = {}
-                    row["episode_group"] = i
-                    row["agent"] = agent
-                    row["reward"] = rewards_per_agent_in_group[agent]
-                    row["n_steps"] = steps_per_agent_in_group[agent]
-                    row["average_reward"] = row["reward"]/row["n_steps"]
-                    rows.append(row)
-        else:
-            raise RuntimeError(f"View {view} unknown!")
 
         import pandas as pd
         self.df = pd.DataFrame(rows)
 
-    def show(self, view_fields = ("reward", "failure")):
+    def show(self, view_fields=("reward", "failure")):
 
         if len(view_fields) < 2:
             raise RuntimeError("Please furnish at least two view fields.")
