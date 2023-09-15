bb447f421168cc092c2b28b00526b9eee3f03d85
Giovanni Sileno <g.sileno@uva.nl>
2023-09-15 23:18:43 +0200

    generalized the dqn example on pytorch for MARL

---
+++ mesa_gym/trainers/DQN.py
@@ -0,0 +1,144 @@
+import gymnasium as gym
+import math
+import random
+from collections import namedtuple, deque
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+# if gpu is to be used
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+Transition = namedtuple('Transition', ('state', 'action', 'next_state', 'reward'))
+
+# Replay memory
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([],maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
+
+
+class DQNTrainer:
+
+    def __init__(self, agent, action_space, observation_space,
+                 replay_batch_size,
+                 discount_factor,
+                 initial_epsilon,
+                 final_epsilon,
+                 epsilon_decay,
+                 update_rate,
+                 learning_rate):
+
+        self.agent = agent
+        self.action_space = action_space
+        self.observation_space = observation_space
+        self.replay_batch_size = replay_batch_size
+        self.discount_factor = discount_factor
+        self.initial_epsilon = initial_epsilon
+        self.final_epsilon = final_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.update_rate = update_rate
+        self.learning_rate = learning_rate
+
+        nb_actions = gym.spaces.flatdim(action_space)
+        nb_states = gym.spaces.flatdim(observation_space)
+
+        self.policy_net = DQN(nb_states, nb_actions).to(device)
+        self.target_net = DQN(nb_states, nb_actions).to(device)
+        self.target_net.load_state_dict(self.policy_net.state_dict())
+
+        self.optimizer = optim.AdamW(self.policy_net.parameters(), lr=self.learning_rate, amsgrad=True)
+        self.memory = ReplayMemory(10000)
+
+        self.steps_done = 0
+
+    def select_action(self, state):
+        sample = random.random()
+        eps_threshold = self.final_epsilon + (self.initial_epsilon - self.final_epsilon) * \
+                        math.exp(-1. * self.steps_done / self.epsilon_decay)
+
+        self.steps_done += 1
+        if sample > eps_threshold:
+            with torch.no_grad():
+                # t.max(1) will return the largest column value of each row.
+                # second column on max result is index of where max element was
+                # found, so we pick action with the larger expected reward.
+                return self.policy_net(state).max(1)[1].view(1, 1)
+        else:
+            return torch.tensor([[self.action_space.sample()]], device=device, dtype=torch.long)
+
+
+    def optimize_model(self):
+
+        if len(self.memory) < self.replay_batch_size:
+            return
+        transitions = self.memory.sample(self.replay_batch_size)
+
+        # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
+        # detailed explanation). This converts batch-array of Transitions
+        # to Transition of batch-arrays.
+        batch = Transition(*zip(*transitions))
+
+        # Compute a mask of non-final states and concatenate the batch elements
+        # (a final state would've been the one after which simulation ended)
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=device, dtype=torch.bool)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+        state_batch = torch.cat(batch.state)
+        action_batch = torch.cat(batch.action)
+        reward_batch = torch.cat(batch.reward)
+
+        # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
+        # columns of actions taken. These are the actions which would've been taken
+        # for each batch state according to policy_net
+        state_action_values = self.policy_net(state_batch).gather(1, action_batch)
+
+        # Compute V(s_{t+1}) for all next states.
+        # Expected values of actions for non_final_next_states are computed based
+        # on the "older" target_net; selecting their best reward with max(1)[0].
+        # This is merged based on the mask, such that we'll have either the expected
+        # state value or 0 in case the state was final.
+        next_state_values = torch.zeros(self.replay_batch_size, device=device)
+        with torch.no_grad():
+            next_state_values[non_final_mask] = self.target_net(non_final_next_states).max(1)[0]
+        # Compute the expected Q values
+        expected_state_action_values = (next_state_values * self.discount_factor) + reward_batch
+
+        # Compute Huber loss
+        criterion = nn.SmoothL1Loss()
+        loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+        # Optimize the model
+        self.optimizer.zero_grad()
+        loss.backward()
+        # In-place gradient clipping
+        torch.nn.utils.clip_grad_value_(self.policy_net.parameters(), 100)
+        self.optimizer.step()
